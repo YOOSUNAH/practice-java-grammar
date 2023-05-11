ee4157d0c8079f824cdaa4444a084a22709d2960
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-05-11 23:50:29 +0900

    문자열_주민등록번호에서 생년월일 및 성별까지만 출력하는 프로그램

--- src/chap_03/_01_String1.java
+++ src/chap_03/_01_String1.java
@@ -13,7 +13,7 @@ public class _01_String1 {
         System.out.println(s.toLowerCase()); //소문자로 변경
 
         //포함 관계
-        System.out.println(s.contains("Java"));//포함된다면 true /포함되지않는다면false
+        System.out.println(s.contains("Java"));//포함된다면 true /포함되지 않는다면 false
         System.out.println(s.contains("C#")); //false
         System.out.println(s.indexOf("Java")); //위치정보 7. 0부터시작함
         System.out.println(s.indexOf("C#")); //포함되지 않는다면 -1.
@@ -21,7 +21,6 @@ public class _01_String1 {
         System.out.println(s.lastIndexOf("and"));// 마지막 일치하는 위치 정보 23.
         System.out.println(s.startsWith("I like")); // 이 문자열로 시작하면 true(아니면 false)
         System.out.println(s.endsWith(".")); //이 문자열로 끝나면 true (아니면 false)
-
 
 
     }
