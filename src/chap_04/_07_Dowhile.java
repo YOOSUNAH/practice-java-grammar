67d63c4a31301d705fcf6ddddf10725405d4af5d
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-05-13 00:08:19 +0900

    반복문_Do While 반복문

---
+++ src/chap_04/_07_Dowhile.java
@@ -0,0 +1,39 @@
+package chap_04;
+
+public class _07_Dowhile {
+    public static void main(String[] args) {
+        //반복문 DoWhile
+        int distance = 25; // 전체 거리 25m
+        int move = 0; // 현재 이동 거리 0m
+        int height = 2; // 키 2m
+        while (move + height < distance) {
+            System.out.println("발차기를 계속 합니다.");
+            System.out.println("현재 이동 거리 : " + move);
+            move += 3; // 3미터 이동
+        }
+        System.out.println("도착했습니다.");
+        System.out.println(" ---반복문 #1 ---");
+
+        // 키가 엄청나게 큰 사람?
+        move = 0;
+        height = 25; //키가 25m
+        while (move + height < distance) {
+            System.out.println("발차기를 계속 합니다.");
+            System.out.println("현재 이동 거리 : " + move);
+            move += 3; // 3미터 이동
+        }
+        System.out.println("도착했습니다.");
+
+        System.out.println(" ---반복문 #2 ---");
+
+        // Do While 반복문
+        // do{ } while(조건); 먼저 수행하고, 조건보고 계속할지 결정함.최소 한번은 함
+        do{
+            System.out.println("발차기를 계속 합니다.");
+            System.out.println("현재 이동 거리 : " + move);
+            move += 3;
+        } while (move + height < distance); //28은 25보다 작지 않아 탈출
+        System.out.println("도착했습니다.");
+
+    }
+}
