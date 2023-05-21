cee0e46fa7c325a1a7d15c39f9ba74fa4c632b59
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-05-21 17:13:35 +0900

    class_Getter & Setter

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -19,19 +19,20 @@ public class BlackBox {
     static boolean canAutoReport = false; // 자동 신고 기능
 
     //생성자 _ 클래스명과 똑같이 만들어주면 됨, 반환형이 따로 없음.
-    BlackBox(){
-        System.out.println("기본 생성자 호출");
-        this.serialNumber = ++counter;
-        System.out.println("새로운 시리얼 넘버를 발급받았습니다. : " + this.serialNumber);
+    BlackBox() {
+//        System.out.println("기본 생성자 호출");
+//        this.serialNumber = ++counter;
+//        System.out.println("새로운 시리얼 넘버를 발급받았습니다. : " + this.serialNumber);
     }
-    BlackBox(String modelName, String resoultion, int price, String color) {
-        this(); // 기본생성자 호출 // 기본 생성자를 바로 접근 , 위에 같은 색깔의 BlackBox 생성자 동작을 바로 실행
 
-        System.out.println("사용자 정의 생성자 호출");
-        this.modelName = modelName;
-        this.resoultion = resoultion;
-        this.price = price;
-        this.color = "블랙";
+    BlackBox(String modelName, String resoultion, int price, String color) {
+//        this(); // 기본생성자 호출 기본 생성자를 바로 접근 , 위에 같은 색깔의 BlackBox 생성자 동작을 바로 실행
+//
+//        System.out.println("사용자 정의 생성자 호출");
+//        this.modelName = modelName;
+//        this.resoultion = resoultion;
+//        this.price = price;
+//        this.color = "블랙";
     }
 
 
@@ -79,12 +80,52 @@ public class BlackBox {
     static void callServiceCenter() {
         System.out.println("서비스 센터(1588-0000)로 연결합니다.");
         //(불가능) modelName = "test;  // 인스턴스 변수는 객체가 만들어져야 만들어지는 변수이기 때문에, static 메소드에서는 직접접근이 불가능하다.
-       //(가능) canAutoReport = false;  // static으로 선언한 class 변수는 static 메소드에서 바로 사용할수 있다.
+        //(가능) canAutoReport = false;  // static으로 선언한 class 변수는 static 메소드에서 바로 사용할수 있다.
 
     }
 
-     void appendModelName(String modelName) {
-       this.modelName += modelName; // class가 가지고 있는 인스턴스 변수에 modelName을 명시하고 싶으면 this. 을 붙이면 됨. 그럼 class내 인스턴스 변수에 직접 접근하게 됨
+    void appendModelName(String modelName) {
+        this.modelName += modelName; // class가 가지고 있는 인스턴스 변수에 modelName을 명시하고 싶으면 this. 을 붙이면 됨. 그럼 class내 인스턴스 변수에 직접 접근하게 됨
         // this. 이 없는 modelName은 파라미터로 전달 받은 (String modelName)에서 modelName
+    }
+
+    // Getter & Setter
+    // Getter 는 값을 가지고 오는 메소드
+    // Setter 는 값을 설정하는 메소드
+    String getModelName() {  // get으로 시작하는 getter
+        return modelName;
+    }
+
+    void setModelName(String modelName) {  //(전달받는 값)  // set으로 시작하는 setter
+        this.modelName = modelName;
+    }
+
+    String getResoultion() {
+        if (resoultion == null || resoultion.isEmpty()) {
+            return "판매자에게 문의하세요.";
+        }
+        return resoultion;  // resoultion 값이 없었어
+    }
+
+    void setResoultion(String resoultion) {
+        this.resoultion = resoultion;
+    }
+
+    int getPrice() {
+        return price;
+    }
+
+    void setPrice(int price) {
+        if (price < 100000) {
+            this.price = 100000;
+        } else {
+            this.price = price;
+        }
+    }
+    String getColor(){
+        return color;
+    }
+    void setColor(String color){
+        this.color = color;
     }
 }
