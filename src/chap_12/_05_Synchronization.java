96dd25b7c66ac2d64fe753c8763b896bcf306ee3
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-05-28 08:19:08 +0900

    Synchronization_순서대로

---
+++ src/chap_12/_05_Synchronization.java
@@ -0,0 +1,38 @@
+package chap_12;
+
+import chap_12.clean.Room;
+
+public class _05_Synchronization {
+    public static void main(String[] args) {
+        Room room = new Room();
+        Runnable cleaner1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("-- 직원1 청소 시작 --");
+                for (int i = 1; i <= 5; i++) {
+                    room.clean("직원1");
+                }
+                System.out.println("-- 직원1 청소 끝 --");
+            }
+        };
+
+        Runnable cleaner2 = () -> {
+            System.out.println("-- 직원2 청소 시작 --");
+            for (int i = 1; i <= 5; i++) {
+                room.clean("직원2");
+            }
+            System.out.println("-- 직원2 청소 끝 --");
+        };
+
+        Thread cleanerThread1 = new Thread(cleaner1);
+        Thread cleanerThread2 = new Thread(cleaner2);
+
+        cleanerThread1.start();
+        cleanerThread2.start();
+
+
+    }
+}
+
+
+// 2개의 Thread 중 하나가 문제가 생겨도, 다른 하나는 잘 돌아감
