4d2e3ae74ed04b506c9f1360d90f5cfc1562f37a
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-05-28 07:14:19 +0900

    예외처리하는 방법_quiz

---
+++ src/chap_11/_Quiz_11.java
@@ -0,0 +1,46 @@
+package chap_11;
+
+// 2가지 문제: 상품 구매가능시간, 상품 매진
+// 각 문제에 대한 예외 클래스 작성
+// 에러 코드에 따른 의도적 예외 발생 및 예외 처리
+
+// 에러코드 / 의미 / 예외 객체 생성 메시지 / 예외 처리 메시지
+// 코드 0 / 의미 에러 없음 / (상품 구매를 완료하였습니다.)
+// 코드 1 / 판매 시간 아님 / 상품 구매 가능 시간이 아닙니다. / 상품 구매는 20시부터 가능합니다.
+// 코드 2 / 매진 / 해당상품은 매진되었습니다. / 다음 기회에 이용해주세요
+
+public class _Quiz_11 {
+    public static void main(String[] args) {
+        int errorCode = 2;
+
+        try {
+            if (errorCode == 0){
+                System.out.println("상품 구매를 완료하였습니다.");
+            } else if (errorCode == 1){
+                throw new NotOnSaleException("상품 구매 가능 시간이 아닙니다.");
+            } else if (errorCode == 2){
+                throw new SoldOutException("해당 상품은 매진되었습니다.");
+            }
+
+        }catch (NotOnSaleException e){
+            System.out.println(e.getMessage());
+            System.out.println("상품 구매는 20시부터 가능합니다.");
+        }catch (SoldOutException e){
+            System.out.println(e.getMessage());
+            System.out.println("다음 기회에 이용해주세요");
+
+        }
+    }
+}
+
+class NotOnSaleException extends Exception{
+    public NotOnSaleException(String message) {
+        super(message);
+    }
+}
+
+class SoldOutException extends Exception{
+    public SoldOutException(String message) {
+        super(message);
+    }
+}
