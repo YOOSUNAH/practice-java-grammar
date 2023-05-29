0d6b364f01f45152067d98a9985f233fa99b57c7
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-05-29 22:19:55 +0900

    Output 출력 _정수, 실수_실수예시 System.out.printf("%f%n", Math.PI);

--- src/chap_13/_02_Output.java
+++ src/chap_13/_02_Output.java
@@ -21,5 +21,14 @@ public class _02_Output {
         System.out.printf("%,15d%n", 1000000000); // __1,000,000,000
         System.out.printf("%-6d%n", 1234); // 1234__ (6자리 공간을 확보하고 나서 왼쪽 정렬 1234출력)
 
+        System.out.println("-------- 실수 --------"); // %f (floating-point: 부동 소수점 형식)
+        System.out.printf("%f%n", Math.PI); // 3.14593 (일반출력) //PI = 3.141593...
+        System.out.printf("%.2f%n", Math.PI); // 3.14  %2f 소수점 둘째 자리까지, 세째자리에서 반올림
+        System.out.printf("%6.2f%n", Math.PI); // __3.14 6자리만큼 공간을 마련하고 나서 소수점 둘째자리까지 실수값을 출력
+        System.out.printf("%-6.2f%n", Math.PI); // 3.14__ // 마이너스 붙이면 왼쪽 정렬
+        System.out.printf("%06.2f%n", Math.PI); // 003.14 //6자리 공간확보, 소수점둘째자리까지 출력, 빈자리는 0을로 출력
+        System.out.printf("%+6.2f%n", Math.PI); //_+3.14  // 항상+-기호표시 //원래 양수면 플러스 기호 생략하는데 표시하려면 + 붙임
+
+
     }
 }
